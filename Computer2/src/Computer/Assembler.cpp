69069a62176cca8ecdae712164c0306dd2cac1c4
Bruce Cosgrove <1901.brc@gmail.com>
2022-09-25 00:22:10 -0700

    See desc.
    1) Added basic assembler error and warning codes.
    2) Made tokenization also spit out label definitions in the same pass using tokenization data, i.e. no extra overhead. In fact, it's effectively less overhead since it's in the same pass.
    
    TODO:
    1) Haven't tested line ending calculator for CRLF nor CR line endings.

--- Computer2/src/Computer/Assembler.cpp
+++ Computer2/src/Computer/Assembler.cpp
@@ -1,27 +1,69 @@
 #include "Assembler.h"
+#include <cassert>
 #include <cctype>
+#include <iostream>
 #include <vector>
 
-void Assembler::Assemble(std::string_view source)
+constexpr bool IsLineEnd(char c) noexcept
+{
+	return c == '\n' || c == '\r';
+}
+
+AssemblerOutput Assembler::Assemble(std::string_view source)
 {
 	if (source.empty())
-		return;
+		return AssemblerReturnCode_EffectivelyEmptySource;
 
+	struct Label
+	{
+		std::string_view name;
+		size_t lineNumber = 0;
+	};
+
+	std::vector<Label> labels;
 	std::vector<std::vector<std::string_view>> tokens;
 	{
 		// Separate source code into lines while ignoring preceding whitespace, traling whitespace, and comments.
-		std::vector<std::string_view> lines;
+		struct Line
+		{
+			std::string_view contents;
+			size_t number = 0;
+		};
+
+		std::vector<Line> lines;
 		{
 			size_t lastSourceIndex = source.size() - 1;
-			size_t lineBegin = 0;
 
+			// Get number of characters in line endings.
+			size_t lineEndingCharCount = 1;
+			{
+				char c = source[0];
+				size_t i = 0;
+				// Find the end of the first line.
+				while (!IsLineEnd(c) && i < lastSourceIndex)
+					c = source[++i];
+
+				if (i >= lastSourceIndex)
+					return AssemblerReturnCode_EffectivelyEmptySource;
+
+				// Check if the line ending is CRLF.
+				if (c == '\r' && source[i + 1] == '\n')
+					lineEndingCharCount++;
+			}
+
+			size_t lineEndingsEncountered = 0;
+			size_t lineBegin = 0;
 			while (true)
 			{
 				// Get first index of a non-whitespace character.
 				size_t i = lineBegin;
 				char c = source[i];
 				while (std::isspace(c) && i < lastSourceIndex)
+				{
+					if (IsLineEnd(c))
+						lineEndingsEncountered++;
 					c = source[++i];
+				}
 
 				// If there's no more source code, stop looking.
 				if (i >= lastSourceIndex)
@@ -34,7 +76,7 @@ void Assembler::Assemble(std::string_view source)
 					lineBegin = i;
 
 					// Find the end of the line.
-					while (c != ';' && c != '\n' && c != '\r' && i < lastSourceIndex)
+					while (c != ';' && !IsLineEnd(c) && i < lastSourceIndex)
 						c = source[++i];
 
 					// Remove trailing whitespace.
@@ -42,7 +84,13 @@ void Assembler::Assemble(std::string_view source)
 					while (lineEnd > lineBegin && std::isblank(source[--lineEnd]));
 
 					// Add the line to the list of lines.
-					lines.emplace_back(source.begin() + lineBegin, source.begin() + (lineEnd + 1));
+					size_t lineNumber = lineEndingsEncountered / lineEndingCharCount + 1;
+					if (lineEndingCharCount == 2)
+						assert(lineEndingsEncountered % 2 == 0);
+					lines.emplace_back(std::string_view(source.begin() + lineBegin, source.begin() + (lineEnd + 1)), lineNumber);
+
+					if (IsLineEnd(c))
+						lineEndingsEncountered++;
 
 					// If there's no more source code, stop looking.
 					if (i >= lastSourceIndex)
@@ -53,17 +101,22 @@ void Assembler::Assemble(std::string_view source)
 				if (c == ';')
 				{
 					do c = source[++i];
-					while (c != '\n' && c != '\r' && i < lastSourceIndex);
+					while (!IsLineEnd(c) && i < lastSourceIndex);
 
 					// If there's no more source code, stop looking.
 					if (i >= lastSourceIndex)
 						break;
+
+					lineEndingsEncountered++;
 				}
 				lineBegin = i + 1;
 			}
+
+			if (lines.empty())
+				return AssemblerReturnCode_EffectivelyEmptySource;
 		}
 
-		// Tokenize each line.
+		// Tokenize each line and find all label definitions.
 		{
 			// test_program.asm reaches here with correct lines.
 			// Now, tokenizing the line will work differently depending on the type of line.
@@ -74,8 +127,68 @@ void Assembler::Assemble(std::string_view source)
 			// then the rest of the line.
 
 			// Also worth noting is that anything that expects an integer requires a constexpr evaluation,
-			// i.e. using 23 in place of "17 + 6". This includes adding numbers represented in any supported base,
-			// which are binary, decimal, and hexidecimal.
+			// i.e. using 23 in place of "17 + 6". This includes multiple numbers represented in any supported base,
+			// which are binary, decimal, and hexadecimal.
+
+			for (const auto& [line, lineNumber] : lines)
+			{
+				// If the line could be a label definition...
+				if (line.ends_with(':'))
+				{
+					if (line.size() > 1)
+					{
+						// Evaluate this regex on line without using a regex because C++'s std::regex is ironically slow.
+						// "^[._a-zA-Z][._a-zA-Z0-9]*:$"
+
+						// If the first character in the label is valid...
+						if (std::isalpha(line.front()) || line.front() == '.' || line.front() == '_')
+						{
+							// If the rest of the characters in the label are valid...
+							for (size_t j = 1; j < line.size() - 1; j++)
+							{
+								char c = line[j];
+								if (!(std::isalnum(c) || c == '.' || c == '_'))
+									return { AssemblerReturnCode_InvalidLabelDefinition, lineNumber };
+							}
+
+							std::string_view newLabel = line.substr(0, line.size() - 1);
+
+							// If the label doesn't yet exist...
+							for (auto& [label, index] : labels)
+								if (label == newLabel)
+									return { AssemblerReturnCode_DuplicateLabelDefinition, lineNumber };
+
+							// Label definition is valid, so add it to the list of labels.
+							labels.emplace_back(newLabel, lineNumber);
+						}
+						else
+							return { AssemblerReturnCode_InvalidLabelDefinition, lineNumber };
+					}
+					else
+						return { AssemblerReturnCode_EmptyLabelDefinition, lineNumber };
+				}
+				// If the line could be an assembler directive...
+				else if (line.starts_with('.'))
+				{
+					std::string_view directive = line.substr(1);
+					directive = directive.substr(0, directive.find(' '));
+
+					if (directive == "org")
+					{
+
+					}
+					else if (directive == "db")
+					{
+
+					}
+					else if (directive == "dw")
+					{
+
+					}
+				}
+			}
 		}
 	}
+
+	return AssemblerReturnCode_Success;
 }
--- Computer2/src/Computer/Assembler.h
+++ Computer2/src/Computer/Assembler.h
@@ -2,10 +2,41 @@
 
 #include <string_view>
 
+using AssemblerReturnCode = uint16_t;
+enum AssemblerReturnCode_ : AssemblerReturnCode
+{
+	AssemblerReturnCode_Success = 0,
+
+	// Warnings
+	AssemblerReturnCode_EffectivelyEmptySource = 0x1000,
+
+	// Errors
+	AssemblerReturnCode_EmptyLabelDefinition = 0x2000,
+	AssemblerReturnCode_InvalidLabelDefinition,
+	AssemblerReturnCode_DuplicateLabelDefinition,
+};
+
+struct AssemblerOutput
+{
+	AssemblerReturnCode code = AssemblerReturnCode_Success;
+	size_t line = 0;
+
+	constexpr AssemblerOutput() noexcept = default;
+	constexpr AssemblerOutput(AssemblerReturnCode code, size_t line = 0) noexcept
+		: code(code), line(line) {}
+
+	constexpr operator bool() const noexcept
+	{
+		return code == AssemblerReturnCode_Success;
+	}
+};
+
 class Assembler
 {
 public:
-	static void Assemble(std::string_view source);
+	// Assumes line endings are one of LF, CRLF, or CR, and are consistent.
+	// Otherwise, you could get errors reported on incorrect lines.
+	static AssemblerOutput Assemble(std::string_view source);
 private:
 	Assembler() = delete;
 	Assembler(const Assembler&) = delete;
