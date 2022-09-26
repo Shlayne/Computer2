c414b765135fe7dcf781b553bac4f88f9cd31e8d
Bruce Cosgrove <1901.brc@gmail.com>
2022-09-26 00:09:39 -0700

    Added include statement for future testing.

--- Computer2/test/test_program.asm
+++ Computer2/test/test_program.asm
@@ -1,11 +1,12 @@
-	.org $8000
-
+.include "other_file.inc"
+.org $8000
+Start:
 	ldi a, 17 + 6 ; sum is 23
 	ldi b,20-3;difference is            17
 	add b                  ; sum is 40   
 	sto [Result], a
 	halt
 
-	.org $8100
+.org $8100
 Result:
 	.db 0
